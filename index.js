484eeba80405746e51a6d11f59e4a51a70ec13e6
AleksandrRazhev <alexandrrazh@gmail.com>
2022-01-25 20:54:51 +0300

    added: db, html

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const express = require('express')
 const app = express()
-const data = require ('./data/data.json')
+const db = require ('./data/db.json')
 
 const PORT = process.env.PORT || 80
+// const PORT = 3000
 
 app.get('/', (req, res) => {
-  res.end('<h1>Home Page</h1>')
+  // res.end('<h1>Home Page</h1>')
+  res.sendFile(__dirname + "/static/index.html")
 })
 
 app.get('/data/', (req, res) => {
-  res.end(JSON.stringify(data.data))
+  res.end(JSON.stringify(db.data))
+})
+
+app.get('/posts/', (req, res) => {
+  res.end(JSON.stringify(db.posts))
 })
 
 app.listen(PORT, () => {
